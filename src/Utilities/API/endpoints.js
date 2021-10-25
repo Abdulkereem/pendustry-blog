7b542f6a46dc0fea21a1afb60a2baae02d9d94d7
Mrbeyond <beyondbeyond602@gmail.com>
2021-10-25 17:13:08 +0100

    Wanna test server backend with netlify version

--- src/Utilities/API/endpoints.js
+++ src/Utilities/API/endpoints.js
@@ -1,12 +1,12 @@
 export const API="https://pend.hiosoft.com.ng/";
 
-export const LAPI="http://localhost:2021/api/";
-export const UAPI="http://localhost:2021/auth/";
-export const banner_url="http://localhost:2021/banners/";
-export const dp_url="http://localhost:2021/dps/";
+// export const LAPI="http://127.0.0.1:2021/api/";
+// export const UAPI="http://127.0.0.1:2021/auth/";
+// export const banner_url="http://127.0.0.1:2021/banners/";
+// export const dp_url="http://127.0.0.1:2021/dps/";
 
-// export const LAPI="https://pendustry.herokuapp.com/api/";
-// export const UAPI="https://pendustry.herokuapp.com/auth/";
-// export const banner_url="https://pendustry.herokuapp.com/banners/";
-// export const dp_url="https://pendustry.herokuapp.com/dps/";
+export const LAPI="https://pendustry.herokuapp.com/api/";
+export const UAPI="https://pendustry.herokuapp.com/auth/";
+export const banner_url="https://pendustry.herokuapp.com/banners/";
+export const dp_url="https://pendustry.herokuapp.com/dps/";
 
